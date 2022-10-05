9279ec204a48c160953558867e5f7ee0e9f6138a
Temich93s <nil>
2022-10-05 22:29:45 +1000

    init commit

---
+++ magazineApp/magazineApp/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  magazineApp
+//
+//  Created by 2lup on 05.10.2022.
+//
+
+import UIKit
+
+/// SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session:
+        UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard (scene as? UIWindowScene) != nil else { return }
+    }
+
+}
---
+++ magazineApp/magazineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magazineApp
+//
+//  Created by 2lup on 05.10.2022.
+//
+
+import UIKit
+
+/// ViewController
+class ViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
